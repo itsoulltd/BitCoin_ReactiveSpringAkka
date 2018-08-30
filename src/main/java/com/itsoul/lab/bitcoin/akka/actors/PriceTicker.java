84074fa95a08a5f41ac709ac73a0f1248d6208e0
Towhid Islam <m.towhid.islam@gmail.com>
2018-08-30 18:30:09 +0600

    Refactoring.

--- src/main/java/com/itsoul/lab/bitcoin/akka/actors/PriceTicker.java
+++ src/main/java/com/itsoul/lab/bitcoin/akka/actors/PriceTicker.java
@@ -3,27 +3,26 @@ package com.itsoul.lab.bitcoin.akka.actors;
 import akka.actor.AbstractActorWithTimers;
 import akka.actor.ActorRef;
 import akka.actor.Props;
-import com.itsoul.lab.bitcoin.akka.messages.CryptoPrice;
 import com.itsoul.lab.bitcoin.akka.messages.FirstTick;
 import com.itsoul.lab.bitcoin.akka.messages.GetThisCryptoPrice;
 import com.itsoul.lab.bitcoin.akka.messages.Tick;
 
 import java.time.Duration;
 
-public class Poller extends AbstractActorWithTimers {
+public class PriceTicker extends AbstractActorWithTimers {
 
     private static final Object TICK_KEY = "TickKey";
     private final ActorRef requestorActor;
     private final String cryptoName;
 
-    public Poller(ActorRef requestorActor, String cryptoName) {
+    public PriceTicker(ActorRef requestorActor, String cryptoName) {
         this.requestorActor = requestorActor;
         this.cryptoName = cryptoName;
         getTimers().startSingleTimer(TICK_KEY, new FirstTick(), Duration.ofMillis(3000));
     }
 
     public static Props props(String cryptoName, ActorRef requestorActor){
-        return Props.create(Poller.class, () -> new Poller(requestorActor, cryptoName));
+        return Props.create(PriceTicker.class, () -> new PriceTicker(requestorActor, cryptoName));
     }
 
     @Override
--- src/main/java/com/itsoul/lab/bitcoin/akka/cmd/AkkaCMDLineUI.java
+++ src/main/java/com/itsoul/lab/bitcoin/akka/cmd/AkkaCMDLineUI.java
@@ -1,6 +1,6 @@
 package com.itsoul.lab.bitcoin.akka.cmd;
 
-import com.itsoul.lab.bitcoin.akka.actors.Poller;
+import com.itsoul.lab.bitcoin.akka.actors.PriceTicker;
 import com.itsoul.lab.bitcoin.akka.actors.PriceRequestor;
 
 import akka.actor.ActorRef;
@@ -40,8 +40,8 @@ public class AkkaCMDLineUI implements CommandLineRunner {
     final ActorRef printer = system.actorOf(Printer.props(), "printer");
     final ActorRef priceRequestor = system.actorOf(PriceRequestor.props(printer, coinbaseService), "requestor");
 
-    final ActorRef poller_USD = system.actorOf(Poller.props("BTC-USD", priceRequestor), "poller-USD");
-    final ActorRef poller_ETH = system.actorOf(Poller.props("ETH-USD", priceRequestor), "poller-ETH");
+    final ActorRef poller_USD = system.actorOf(PriceTicker.props("BTC-USD", priceRequestor), "poller-USD");
+    final ActorRef poller_ETH = system.actorOf(PriceTicker.props("ETH-USD", priceRequestor), "poller-ETH");
 
   }
 
